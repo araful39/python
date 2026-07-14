22c3918e4e7d6c3dee3c5300976d04f160497044
araful <rajuslam39@gmail.com>
2026-07-14 14:15:20 +0600

    to day i learn and practics python str like string

---
+++ str.py
@@ -0,0 +1,127 @@
+# name ="Araful"
+# age =27
+
+# print(name,age)
+
+# message = """
+# Hello,
+# I am learning Python.
+# I am a Flutter Developer.
+# """
+
+# print(message)
+
+# name = "python"
+
+# print(name[0])
+# name = "Python"
+
+# print(name[::2])
+
+# print(name[::-1])
+
+# name = "Hello World"
+
+# print(len(name))
+
+# name = "Python"
+
+# name = "J" + name[1:]
+
+# print(name)
+
+# name = "Python"
+
+# for char in name:
+#     print(char.upper())
+# text="python programming"
+# print(text.capitalize())
+
+# print(text.title())
+
+# text = "PyThOn"
+
+# print(text.swapcase())
+
+# name = "   Araful   "
+
+# print(name.strip())
+
+# email =input("Email: ").strip()
+
+# print(email)
+
+# replace = email.replace("raju","Araful")
+# print(replace)
+
+# spl =replace.split()
+# print(spl)
+
+# skills = [
+#     "Python",
+#     "Flutter",
+#     "Dart"
+# ]
+
+# result = ", ".join(skills)
+
+# print(result)
+
+# text = "I love Python"
+
+# print(text.find("Python"))
+
+
+name = "Araful"
+age = 27
+
+message = f"My name is {name}. I am {age} years old."
+
+print(message)
+
+
+print(f"My name is {name}")
+
+print("Hello\nPython")
+print("He said \"Python\"")
+print("C:\\Users\\Araful")
+
+path =r"G:\\todo_app"
+print(path)
+
+age =27
+
+age = str(age)
+print(f"{age}")
+
+url = "https://app.com/payment/success?status=paid"
+
+if "status=paid" in url:
+    print("Payment Success")
+
+elif "status=failed" in url:
+    print("Payment Failed")
+
+else:
+    print("Payment Pending")
+
+
+#     String Methods Cheat Sheet
+# Method	কাজ
+# upper()	Uppercase
+# lower()	Lowercase
+# capitalize()	First letter uppercase
+# title()	Each word capitalize
+# strip()	Whitespace remove
+# replace()	Text replace
+# split()	String → List
+# join()	List → String
+# find()	Position find
+# count()	Count
+# startswith()	Start check
+# endswith()	End check
+# isdigit()	Digit check
+# isalpha()	Alphabet check
+# isalnum()	Alphabet + number
+
+# সবচেয়ে বেশি practice করো: slicing, strip(), split(), join(), replace(), startswith(), endswith(), আর f-string।
