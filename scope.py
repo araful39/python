11e6c68ed4104afa52e9bb5d3d956938903c52e1
araful <rajuslam39@gmail.com>
2026-07-14 17:56:46 +0600

    scope add

--- scope.py
+++ scope.py
@@ -1,4 +1,22 @@
-name = "Araful"
+name = "Global"
 
 print(name)
 
+
+
+def show_user():
+    name = "Local"
+
+    print(name)
+
+
+show_user()
+
+
+def localReturn():
+    name="Raju"
+
+    return name
+
+
+print(localReturn())
